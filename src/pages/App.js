41fee4d7558d0ce9fc3c548d5540f6876c46844c
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-23 15:02:33 +0200

    created main page

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import VideoSection from "../components/VideoSection";
+
+function App() {
+
+
+    return (
+            <div className="App" >
+                <h1 className="title">Netflix--</h1>
+                <VideoSection/>
+            </div>
+    );
+}
+
+export default App;
