6819eab329f8e4b27e09e760758df4098aaa2959
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-23 15:05:02 +0200

    added Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import './index.css'
+import App from './pages/App';
+import Details from './pages/Details';
+import {Route, BrowserRouter as Router} from "react-router-dom";
+import RootContextProvider from "./contexts/RootContext";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <RootContextProvider>
+            <Route exact path="/" component={App}/>
+            <Route path="/video/:id" component={Details}/>
+        </RootContextProvider>
+    </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
