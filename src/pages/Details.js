ed743a9de14b7939d23fcc2df11337b4f6cd438c
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-23 19:53:36 +0200

    added video and comment form to details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
+import {RootContext} from "../contexts/RootContext";
+import Video from "../components/Video";
+import VideoLoading from "../components/VideoLoading";
+import styled from "styled-components";
+import CommentForm from "../components/CommentForm";
+
 
 function Details(props) {
+    const videoId = props.match.params.id;
+
+    const {state, fetchVideoById} = useContext(RootContext);
+    const actVideoId = state.selectedVideo.id;
+
+    useEffect(() => {
+        fetchVideoById(videoId);
+    }, [videoId]);
+
+    const video = parseInt(videoId) === actVideoId ? <Video/> : <VideoLoading/>;
+
+    const Page = styled.div`
+        width: 95%;
+        margin: 0 auto;
+    `;
+
+    const Wrapper = styled.div`
+        width: 1100px;
+        margin: 40px 30px;
+    `;
 
     return (
-        <div>
-
-        </div>
+        <Page>
+            <Wrapper>
+                {video}
+                <CommentForm/>
+            </Wrapper>
+        </Page>
     );
 }
 
