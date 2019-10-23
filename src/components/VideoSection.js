b6c2ba726dbd5ca6507db8da96a5ecf84b3309a7
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-23 15:00:31 +0200

    implemented VideoSection component to wrap all videos

---
+++ src/components/VideoSection.js
@@ -0,0 +1,38 @@
+import React, {useContext} from 'react';
+import VideoCard from "./VideoCard";
+import styled from "styled-components";
+
+import {RootContext} from "../contexts/RootContext";
+
+function VideoSection(props) {
+    const {state} = useContext(RootContext);
+    const {videos} = state;
+
+    const videoCards = videos.map((video, i) => <VideoCard key={i} video={video}/>);
+
+    const Section = styled.div`
+        width: 1400px;
+        display: flex;
+        flex-flow: row wrap;
+        margin: 0 auto;
+    `;
+
+    const Wrapper = styled.div`
+        margin: 20px auto;
+        width: 1500px;
+        border-top: 1px solid #ffffff;
+        border-bottom: 1px solid #ffffff;
+        padding: 20px;
+
+    `;
+
+    return (
+        <Wrapper>
+            <Section>
+                {videoCards}
+            </Section>
+        </Wrapper>
+    );
+}
+
+export default VideoSection;
