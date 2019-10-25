c4dc38fbfe0ca5656db4b8164b2b8df789868bcd
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-25 17:55:54 +0200

    refactor to handle new props

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -3,7 +3,7 @@ import Rating from "@material-ui/lab/Rating";
 import Tooltip from "@material-ui/core/Tooltip";
 
 function Rate(props) {
-    const {value, handleChange, handleClick} = props;
+    const {value, handleChange, handleClick, disabled, size, style} = props;
 
     const labels = {
         1: '1',
@@ -12,12 +12,6 @@ function Rate(props) {
         4: '4',
         5: '5',
     };
-
-    const style = {
-        padding: "20px 0",
-        position: "absolute"
-    };
-
 
     const IconContainer = (props) => {
         const { value, ...other } = props;
@@ -29,15 +23,16 @@ function Rate(props) {
     };
 
     return (
-        <div style={style}>
+        <div style={style ? style : null}>
             <Rating
                 name="hover-tooltip"
                 value={value}
-                size={"large"}
+                size={size ? size : "large"}
                 precision={1}
                 onChangeActive={handleChange}
                 onMouseUp={handleClick}
                 IconContainerComponent={IconContainer}
+                disabled={disabled ? disabled : false}
             />
         </div>
     );
