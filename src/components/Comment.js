c27ff3b6229f56b5d98bc418c41556a2ae393a84
adrianFarkas <farkasadrian1996@gmail.com>
2019-10-26 15:27:28 +0200

    modified to handle update

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,18 +1,21 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import Rate from "./Rate";
 import styled from "styled-components";
 import {dark, light} from "../theme";
 import {RootContext} from "../contexts/RootContext";
+import CommentForm from "./CommentForm";
+
 
 function Comment(props) {
-    const {rating, comment} = props.comment;
-    const {state} = useContext(RootContext);
-    const {isLightTheme} = state;
+    const {id, rating, comment} = props.comment;
+    const [editMode, setEdiMode] = useState(false);
+    const {state: {isLightTheme}} = useContext(RootContext);
     const theme = isLightTheme ? light : dark;
 
     const Wrapper = styled.div`
         padding: 10px 0;
         color: ${theme.syntax};
+        position: relative;
     `;
 
     const CommentCard = styled.div`
@@ -24,20 +27,38 @@ function Comment(props) {
     const Text = styled.div`
         font-size: 30px;
         padding-top: 15px;
-        max-width: 920px;
+        width: 920px;
         overflow-wrap: break-word;
     `;
+
+    const moreStyle = {
+        top: 0,
+        right: 0,
+        position: "absolute",
+        paddingTop: 10
+    };
+
+    const recommendation = editMode ?
+        <CommentForm
+            rate={rating}
+            comment={comment}
+            commentId={id}
+            buttonText={"Edit"}/>
+        :
+        <div>
+            <Text>{comment}</Text>
+            <Rate
+                value={rating}
+                disabled={true}
+                size={"medium"}
+                style={{textAlign: "right", marginRight: 10}}/>
+        </div>;
 
     return (
         <Wrapper>
             <CommentCard>
-                <div>Unknown</div>
-                <Text>{comment}</Text>
-                <Rate
-                    value={rating}
-                    disabled={true}
-                    size={"medium"}
-                    style={{textAlign: "right", marginRight: 10}}/>
+                <div style={{paddingBottom: "15px"}}>Unknown</div>
+                {recommendation}
             </CommentCard>
         </Wrapper>
     );
