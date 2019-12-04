b422864f5e59ecc9d825a45a344c1448b7e9ade9
adrianFarkas <farkasadrian1996@gmail.com>
2019-12-04 16:20:32 +0100

    transformed into responsive

--- src/components/VideoLoading.js
+++ src/components/VideoLoading.js
@@ -2,21 +2,18 @@ import React from 'react';
 import {CircularProgress} from "@material-ui/core";
 import styled from "styled-components";
 import {colors} from "../theme";
+import {VideoGrid, VidRow} from "../styled-components/styled";
 
 function VideoLoading(props) {
 
-    const LoadingWrapper = styled.div`
-        width: 1100px;
-        height: 620px;
+    const LoadingWrapper = styled(VidRow)`
         background-color: ${colors.lightGrey2};
     `;
 
     return (
-        <LoadingWrapper>
-            <div style={{width: "50px", margin: "0 auto"}}>
-                <CircularProgress style={{color: colors.claret, margin: "280px auto"}}/>
-            </div>
-        </LoadingWrapper>
+        <VideoGrid>
+            <LoadingWrapper/>
+        </VideoGrid>
     );
 }
 
