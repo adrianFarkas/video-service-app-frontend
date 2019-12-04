99e80b2b0bf2e9717d21425edacb9fc6c3329289
adrianFarkas <farkasadrian1996@gmail.com>
2019-12-04 17:21:48 +0100

    set theme in cookie

--- src/components/util/ThemeSwitcher.js
+++ src/components/util/ThemeSwitcher.js
@@ -3,13 +3,16 @@ import FormControlLabel from "@material-ui/core/FormControlLabel";
 import Switch from "@material-ui/core/Switch";
 import {ThemeContext} from "../../contexts/ThemeContext";
 import {light} from "../../theme";
+import Cookies from 'universal-cookie';
 
 function ThemeSwitcher() {
+    const cookie = new Cookies();
     const {theme, changeTheme} = useContext(ThemeContext);
     const [checked, setChecked] = useState(theme === light);
 
     const handleChange = event => {
         const checked = event.target.checked;
+        cookie.set("theme", checked, { path: '/' });
         setChecked(checked);
         changeTheme();
     };
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,11 +1,12 @@
 import React, {createContext, useState} from 'react';
 import {dark, light} from "../theme";
+import Cookies from "universal-cookie";
 
 export const ThemeContext = createContext();
 
 function ThemeContextProvider(props) {
-
-    const [theme, setTheme] = useState(dark);
+    const cookie = new Cookies();
+    const [theme, setTheme] = useState(JSON.parse(cookie.get("theme")) ? light : dark);
 
     const changeTheme = () => {
         setTheme(theme === dark ? light : dark);
