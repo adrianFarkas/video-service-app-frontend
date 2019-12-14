e14178e2f25b702cc9d76a633236129728d4f7f6
adrianFarkas <farkasadrian1996@gmail.com>
2019-12-14 18:04:53 +0100

    separate form to another component

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,34 +1,9 @@
-import React, {useContext} from 'react';
-import TextField from "@material-ui/core/TextField";
-import {makeStyles} from "@material-ui/core";
+import React from 'react';
 import Navbar from "../navigation/Navbar";
 import styled from "styled-components";
-import {ThemeContext} from "../../contexts/ThemeContext";
-import {colors} from "../../theme";
-import {Link} from "react-router-dom";
+import SignUpForm from "./SignUpForm";
 
 function SignUp() {
-    const {theme} = useContext(ThemeContext);
-
-    const useStyles = makeStyles({
-        root: {
-            width: "100%",
-            color: theme.syntax,
-            // margin: "2vw auto",
-            "& label, label.Mui-focused, .MuiInputBase-root": {
-                color: theme.syntax,
-            },
-            "& .MuiInput-underline:after, .MuiInput-underline:before": {
-                borderBottomColor: theme.syntax,
-            },
-            "& .MuiInput-underline": {
-                '&:hover:not($disabled):not($focused):not($error):before': {
-                    borderBottomColor: `${colors.claret} !important`,
-                },
-            },
-        },
-    });
-    const classes = useStyles();
 
     const SignUpContainer = styled.div`
         position: relative;
@@ -39,18 +14,12 @@ function SignUp() {
         display: flex;
     `;
 
-    const SignUpForm = styled.div`
+    const Form = styled.div`
         width: 60%;
         border: 1px solid #6A0007;
         border-bottom-right-radius: 5px;
         border-top-right-radius: 5px;
         border-left: 0;
-    `;
-
-    const CustomForm = styled.form`
-        margin: 10%;
-        display: grid;
-        grid-template-rows: repeat(4, 6vw);
     `;
 
     const Title = styled.div`
@@ -60,7 +29,7 @@ function SignUp() {
     `;
 
     const Header = styled.div`
-        background-image: url("img/signup.png");
+        background-image: url("img/signup3.png");
         background-repeat: no-repeat;
         background-size: cover;
         border-bottom-left-radius: 5px;
@@ -75,42 +44,6 @@ function SignUp() {
         margin: 20% auto 20% 20%;
     `;
 
-    const SignUpBtn = styled.button`
-        position: relative;
-        width: 40%;
-        min-width: 150px;
-        padding: 5px;
-        color: ${theme.syntax};
-        text-align: center;
-        border-radius: 100px;
-        background: ${theme.background};
-        border: 1px solid transparent;
-        transition: all 0.3s ease-in-out 0s;
-        &:after{
-            position: absolute;
-            top: -4px; bottom: -4px;
-            left: -4px; right: -4px;
-            background: linear-gradient(20deg, #fff 0%, #e2000d 50%, #6A0007 100%);
-            content: '';
-            z-index: -1;
-            border-radius: 100px;
-        }
-        &:hover {
-            background: rgba(0,0,0,0.61);
-            color: #fff;
-        }      
-    `;
-
-    const AuthButtons = styled.div`
-        display: flex;
-        margin-top: 30px;
-     `;
-
-    const LoginLink = styled.div`
-        padding: 5px;
-        margin-left: 30px;
-    `
-
     return (
         <div>
             <Navbar/>
@@ -120,18 +53,9 @@ function SignUp() {
                     <Text className={"signup-text"}>It's much better when you have an account.</Text>
                     <Text className={"signup-text"}>Get yourself one.</Text>
                 </Header>
-                <SignUpForm id={"signup-form"}>
-                    <CustomForm>
-                        <TextField className={classes.root} id="username" label="Username" error={false}/>
-                        <TextField className={classes.root} id="email" label="Email" error={false}/>
-                        <TextField className={classes.root} id="password" label="Password" error={false}/>
-                        <TextField className={classes.root} id="password2" label="Verify Password" error={false}/>
-                        <AuthButtons>
-                            <SignUpBtn id={"signup-btn"} type={"submit"}>Sign Up</SignUpBtn>
-                            <LoginLink>or <Link to={"/sign-in"}>Login</Link></LoginLink>
-                        </AuthButtons>
-                    </CustomForm>
-                </SignUpForm>
+                <Form id={"signup-form"}>
+                    <SignUpForm/>
+                </Form>
             </SignUpContainer>
 
         </div>
---
+++ src/components/auth/SignUpForm.js
@@ -0,0 +1,167 @@
+import React, {useContext, useState} from 'react';
+import TextField from "@material-ui/core/TextField";
+import {Link} from "react-router-dom";
+import styled from "styled-components";
+import {makeStyles} from "@material-ui/core";
+import {colors} from "../../theme";
+import {ThemeContext} from "../../contexts/ThemeContext";
+import axios from "axios";
+
+function SignUpForm() {
+    const {theme} = useContext(ThemeContext);
+    const url = "/auth/sign-up";
+
+    const initialState = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        password: "",
+        confirmationPassword: "",
+    };
+
+    const [formData, setFormData] = useState(initialState);
+
+    const useStyles = makeStyles({
+        root: {
+            width: "100%",
+            color: theme.syntax,
+            "& label, label.Mui-focused, .MuiInputBase-root": {
+                color: theme.syntax,
+            },
+            "& .MuiInput-underline:after, .MuiInput-underline:before": {
+                borderBottomColor: theme.syntax,
+            },
+            "& .MuiInput-underline": {
+                '&:hover:before': {
+                    borderBottomColor: `${colors.claret} !important`,
+                },
+            },
+        },
+    });
+    const classes = useStyles();
+
+    const SignUpBtn = styled.button`
+        position: relative;
+        width: 40%;
+        min-width: 150px;
+        padding: 5px;
+        color: ${theme.syntax};
+        text-align: center;
+        border-radius: 100px;
+        background: ${theme.background};
+        border: 1px solid transparent;
+        transition: all 0.3s ease-in-out 0s;
+        &:after{
+            position: absolute;
+            top: -4px; bottom: -4px;
+            left: -4px; right: -4px;
+            background: linear-gradient(20deg, #fff 0%, #e2000d 50%, #6A0007 100%);
+            content: '';
+            z-index: -1;
+            border-radius: 100px;
+        }
+        &:hover {
+            background: rgba(0,0,0,0.61);
+            color: #fff;
+        }      
+    `;
+
+    const PlaceHolder = styled.div`
+        width: 25%;
+    `;
+
+    const AuthButtons = styled.div`
+        display: flex;
+        margin-top: 30px;
+     `;
+
+    const LoginLink = styled.div`
+        padding: 5px;
+        margin-left: 30px;
+    `;
+
+    const formStyle = {
+        margin: "10%",
+        display: "grid",
+        gridTemplateRows: "repeat(4, 6vw)"
+    };
+
+    const textChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        setFormData({...formData, [field]: value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let {firstName, lastName, email, password} = formData;
+        const userData = {firstName, lastName, email, password};
+        axios.post(url, userData)
+            .then(res => console.log(res.data))
+    };
+
+    return (
+        <div>
+            <form style={formStyle} onSubmit={handleSubmit}>
+                <div style={{display:"flex"}}>
+                    <TextField
+                        required
+                        className={classes.root}
+                        id="first-name"
+                        label="First name"
+                        name="firstName"
+                        onChange={textChange}
+                        value={formData.firstName}
+                    />
+                    <PlaceHolder/>
+                    <TextField
+                        required
+                        className={classes.root}
+                        id="last-name"
+                        label="Last name"
+                        name="lastName"
+                        onChange={textChange}
+                        value={formData.lastName}
+                    />
+                </div>
+                <TextField
+                    required
+                    className={classes.root}
+                    id="email"
+                    label="Email"
+                    error={false}
+                    name="email"
+                    onChange={textChange}
+                    value={formData.email}
+                />
+                <TextField
+                    required
+                    className={classes.root}
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                    onChange={textChange}
+                    value={formData.password}
+                />
+                <TextField
+                    required
+                    className={classes.root}
+                    id="confirmationPassword"
+                    label="Confirm Password"
+                    error={false}
+                    name="confirmationPassword"
+                    type="password"
+                    onChange={textChange}
+                    value={formData.confirmationPassword}
+                />
+                <AuthButtons>
+                    <SignUpBtn id={"signup-btn"} type={"submit"}>Sign Up</SignUpBtn>
+                    <LoginLink>or <Link to={"/sign-in"}>Login</Link></LoginLink>
+                </AuthButtons>
+            </form>
+        </div>
+    );
+}
+
+export default SignUpForm;
