7915a3afdbda8aceaf80d9f7bc3839423456002a
adrianFarkas <farkasadrian1996@gmail.com>
2020-01-31 13:55:31 +0100

    created a base modal for reuse

---
+++ src/components/util/BaseModal.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import Backdrop from "@material-ui/core/Backdrop";
+import Zoom from "@material-ui/core/Zoom";
+import IconButton from "@material-ui/core/IconButton";
+import CloseIcon from "@material-ui/icons/Close";
+import Modal from "@material-ui/core/Modal";
+import styled from "styled-components";
+
+const CustomModal = styled(Modal)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Wrapper = styled.div`
+    width: 50%;
+    @media (max-width: 900px) {
+        width: 100%;
+        margin: 5px;
+    }
+`;
+
+const Selector = styled.div`
+    overflow: auto;
+    max-height: 85vh;
+    border: 2px solid ${props => props.syntax};
+    background-color: ${props => props.transparentBackground};
+    color: ${props => props.syntax};
+    padding: 15px;
+    border-radius: 10px;
+    ::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+const Title = styled.div`
+    font-size: 30px;
+`;
+
+const Head = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 20px;
+    padding-bottom: 15px;
+    color: ${props => props.syntax};
+    border-bottom: 1px solid ${props => props.transparentSyntax};
+    & .MuiButtonBase-root {
+      color: ${props => props.syntax};
+    }
+`;
+
+function BaseModal({theme, title, open, handleClose, children}) {
+    return (
+        <CustomModal
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+                timeout: 500,
+            }}
+        >
+            <Zoom in={open}>
+                <Wrapper>
+                    <Selector className={"transition"} {...theme}>
+                        <Head className={"transition"} {...theme}>
+                            <Title>{title}</Title>
+                            <IconButton onClick={handleClose}>
+                                <CloseIcon/>
+                            </IconButton>
+                        </Head>
+                        {children}
+                    </Selector>
+                </Wrapper>
+            </Zoom>
+        </CustomModal>
+    );
+}
+
+export default BaseModal;
--- src/components/util/ThemeSelector.js
+++ src/components/util/ThemeSelector.js
@@ -1,49 +1,9 @@
 import React, {useContext} from 'react';
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import {makeStyles} from "@material-ui/core";
 import {ThemeContext} from "../../contexts/ThemeContext";
 import styled from "styled-components";
 import Theme from "./Theme";
 import {themes} from "../../theme";
-import Zoom from "@material-ui/core/Zoom";
-import CloseIcon from "@material-ui/icons/Close";
-import IconButton from "@material-ui/core/IconButton";
-
-const Wrapper = styled.div`
-    width: 50%;
-    @media (max-width: 900px) {
-        width: 100%;
-        margin: 5px;
-    }
-`;
-
-const Selector = styled.div`
-    overflow: auto;
-    max-height: 85vh;
-    border: 2px solid ${props => props.syntax};
-    background-color: ${props => props.transparentBackground};
-    color: ${props => props.syntax};
-    padding: 15px;
-    border-radius: 10px;
-    ::-webkit-scrollbar {
-        display: none;
-    }
-`;
-
-const Title = styled.div`
-    font-size: 30px;
-`;
-
-const Head = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 20px;
-    padding-bottom: 15px;
-    color: ${props => props.syntax};
-    border-bottom: 1px solid ${props => props.transparentSyntax};
-`;
+import BaseModal from "./BaseModal";
 
 const Themes = styled.div`
     display: grid;
@@ -60,45 +20,17 @@ const Themes = styled.div`
 function ThemeSelector({open, handleClose}) {
     const {theme} = useContext(ThemeContext);
 
-    const useStyles = makeStyles({
-        modal: {
-            display: 'flex',
-            alignItems: "center",
-            justifyContent: "center"
-        },
-        icon: {
-            color: theme.syntax,
-        },
-    });
-    const classes = useStyles();
-
     return (
-        <Modal
+        <BaseModal
+            theme={theme}
             open={open}
-            onClose={handleClose}
-            closeAfterTransition
-            BackdropComponent={Backdrop}
-            className={classes.modal}
-            BackdropProps={{
-                timeout: 500,
-            }}
+            handleClose={handleClose}
+            title={"Select your theme"}
         >
-            <Zoom in={open}>
-                <Wrapper>
-                    <Selector className={"transition"} {...theme}>
-                        <Head className={"transition"} {...theme}>
-                            <Title>Select your theme</Title>
-                            <IconButton className={classes.icon} onClick={handleClose}>
-                                <CloseIcon/>
-                            </IconButton>
-                        </Head>
-                        <Themes>
-                            {Object.values(themes).map((v, i) => <Theme key={i} {...v}/>)}
-                        </Themes>
-                    </Selector>
-                </Wrapper>
-            </Zoom>
-        </Modal>
+            <Themes>
+                {Object.values(themes).map((v, i) => <Theme key={i} {...v}/>)}
+            </Themes>
+        </BaseModal>
     );
 }
 
